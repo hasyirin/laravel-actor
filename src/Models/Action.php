30809f0b512d6a85a92b42e9e6cc5dbb38645ddb
hasyirin <25414222+hasyirin@users.noreply.github.com>
2025-03-16 20:13:11 +0000

    Fix styling

--- src/Models/Action.php
+++ src/Models/Action.php
@@ -24,7 +24,7 @@ class Action extends Model
     protected function casts(): array
     {
         return [
-            'acted_at' => 'datetime'
+            'acted_at' => 'datetime',
         ];
     }
 
