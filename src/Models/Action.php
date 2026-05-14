38875def362807529057a136974f24f84cdfcb11
hasyirin <25414222+hasyirin@users.noreply.github.com>
2026-05-14 12:26:19 +0000

    Fix styling

--- src/Models/Action.php
+++ src/Models/Action.php
@@ -3,7 +3,6 @@
 namespace Hasyirin\Actor\Models;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
